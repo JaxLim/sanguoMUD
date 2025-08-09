e606b9efbe5b87b89f1da547a46e9288b7e3b20d
JaxLim <847550215@qq.com>
2025-08-09 17:54:05 +0800

    Refactor MainWindow left panel

--- ui_qt/MainWindow.h
+++ ui_qt/MainWindow.h
@@ -3,7 +3,6 @@
 #include <QTimer>
 #include <QPlainTextEdit>
 #include <QPushButton>
-#include <QKeyEvent>
 #include "../core/World.h"
 
 class MainWindow : public QMainWindow {
@@ -11,12 +10,10 @@ class MainWindow : public QMainWindow {
 public:
     MainWindow(QWidget* parent=nullptr);
 
-protected:
-    void keyPressEvent(QKeyEvent* e) override;
 private:
     World world_;
     QPlainTextEdit* log_;
-    QPushButton *btnN_,*btnS_,*btnW_,*btnE_,*btnTalk_,*btnAttack_,*btnSave_,*btnLoad_;
+    QPushButton *btnN_,*btnS_,*btnW_,*btnE_,*btnTalk_,*btnAttack_,*btnSave_,*btnLoad_,*btnClear_;
     QTimer* timer_;
     int tick_=0;
     void append(const QString& s);
